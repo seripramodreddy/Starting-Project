db4684b1ac17044229f9d507a581d50ecdbace33
seripramodreddy <seripramodreddy@gmail.com>
2020-06-04 13:30:46 +0530

    late chnage

--- jnsdnfksdjfd/src/jnsdnfksdjfd/heloowrold.java
+++ jnsdnfksdjfd/src/jnsdnfksdjfd/heloowrold.java
@@ -3,7 +3,7 @@ package jnsdnfksdjfd;
 public class heloowrold {
 	public static void main(String args[])
 	{
-		System.out.println("welcome pramod");
+		System.out.println("welcome pramod111");
 		System.out.println("Super code");
 	}
 
